963228f3202564fbe235e3884c81624cf352dd80
dominx99 <dominx992@gmail.com>
2018-02-11 21:07:19 +0100

    aboutme, skills, config.json (skills), navbar changes, scss changes, layout changes

--- app/routes.php
+++ app/routes.php
@@ -1,5 +1,13 @@
 <?php
 
 $app->get('/', function($request, $response){
-    return $this->view->render($response, 'index.twig');
+    return $this->view->render($response, 'aboutme.twig');
+})->setName('about.me');
+
+$app->get('/skills', function($request, $response){
+    return $this->view->render($response, 'skills.twig');
+})->setName('skills');
+
+$app->get('/app', function(){
+    return 'app';
 });
--- index.php
+++ index.php
@@ -2,7 +2,6 @@
 
 session_start();
 require_once(__DIR__ . '/vendor/autoload.php');
-$config = require_once(__DIR__ . '/app/config.php');
 
 $app = new Slim\App([
     'settings' => [
@@ -11,13 +10,22 @@ $app = new Slim\App([
 ]);
 $container = $app->getContainer();
 
-$container['view'] = function($container) use ($config){
+$container['config'] = function($container){
+    return $config = require_once(__DIR__ . '/app/config.php');
+};
+
+$container['data'] = function($container){
+    return json_decode(file_get_contents(__DIR__ . '/src/config.json'), true);
+};
+
+$container['view'] = function($container){
     $view = new Slim\Views\Twig('src/views', [
         //'cache' => 'path/to/cache',
     ]);
 
     $view->addExtension(new Slim\Views\TwigExtension($container->router, $container->request->getUri()));
-    $view->getEnvironment()->addGlobal('config', $config);
+    $view->getEnvironment()->addGlobal('config', $container->config);
+    $view->getEnvironment()->addGlobal('data', $container->data);
 
     return $view;
 };
