bdeda27aa99e003a04fc79926492b07803ae9ba6
dominx99 <dominx992@gmail.com>
2018-02-11 15:20:54 +0100

    routes fix

--- index.php
+++ index.php
@@ -4,21 +4,24 @@ session_start();
 require_once(__DIR__ . '/vendor/autoload.php');
 $config = require_once(__DIR__ . '/app/config.php');
 
-$app = new Slim\App();
+$app = new Slim\App([
+    'settings' => [
+        'displayErrorDetails' => true,
+    ]
+]);
 $container = $app->getContainer();
 
-require_once(__DIR__ . '/app/routes.php');
-
 $container['view'] = function($container) use ($config){
-    $view = new Slim\Views\Twig('src\views', [
+    $view = new Slim\Views\Twig('src/views', [
         //'cache' => 'path/to/cache',
     ]);
 
-    $basePath = rtrim(str_replace('index.php', '', $container['request']->getUri()->getBasePath()), '/');
-    $view->addExtension(new Slim\Views\TwigExtension($container['router'], $basePath));
+    $view->addExtension(new Slim\Views\TwigExtension($container->router, $container->request->getUri()));
     $view->getEnvironment()->addGlobal('config', $config);
 
     return $view;
 };
+
+require_once(__DIR__ . '/app/routes.php');
 
 $app->run();
