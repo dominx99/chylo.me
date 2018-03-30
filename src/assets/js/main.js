7dd2ac65f9ea7dd1d287b3bfddc4d70f08ff7ce5
dominx99 <dominx992@gmail.com>
2018-03-30 12:40:08 +0200

    bigpicture.js gallery

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,10 +1,19 @@
 var images = document.querySelectorAll('img.big-picture');
 
-Array.prototype.forEach.call(images, function(item, i){
-    item.addEventListener('click', function(){
+Array.prototype.forEach.call(images, function(item, i) {
+    item.addEventListener('click', function(e){
+        e.preventDefault();
+
+        var gallery = null;
+
+        if (!e.target.classList.contains('single')) {
+            gallery = '.big-picture-gallery';
+        }
+
         BigPicture({
           el: this,
-          imgSrc: this.getAttribute('data-src')
+          imgSrc: this.getAttribute('data-src'),
+          gallery: gallery
         });
     });
 });
